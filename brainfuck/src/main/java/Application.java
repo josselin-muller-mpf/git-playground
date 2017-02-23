edb5ce7a6f8cb8a82dee821f4006388cd130ec8b
Josselin Muller <josselin.muller@groupm.com>
2017-02-23 18:55:07 -0500

    Adding project sample structure

---
+++ brainfuck/src/main/java/Application.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Josselin.Muller on 2/23/2017.
+ */
+public class Application
+{
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        for (String arg : args)
+            System.out.println(arg);
+    }
+}
